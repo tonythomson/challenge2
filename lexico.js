c32276dc6f2c357fa576d4c0b07b1fa570a14b1b
Tony Thomson <thomson@me.com>
2015-12-31 12:51:41 -0800

    Initial commit

---
+++ lexico.js
@@ -0,0 +1,24 @@
+// Summary: Write a function to sort an array of strings based on an arbitrary
+// lexicographic ordering. The function will take two parameters: an array of
+// strings to sort and a string specifying the lexicographic order.
+
+// Example input #1: ( ["acb", "abc", "bca"], "abc")
+// Example output #1: ["abc","acb","bca"]
+
+// Example input #2: ( ["acb", "abc", "bca"], "cba")
+// Example output #2: ["bca", "acb", "abc"]
+
+// Example input #3: (["aaa","aa",""], "a")
+// Example output #3: ["", "aa", "aaa"]
+
+// You may assume that the strings to be sorted consist only of characters from
+// the specified lexicographical ordering. You may also assume that the
+// characters in the strings to sort consist only of lowercase a-z.
+
+function lexicoSort(arrayOfStrings, lexicoOrder) {
+  'use strict';
+
+
+
+
+};
