19de8278ad30713aaabaa5a74a55f0768e1a93b4
Tony Thomson <thomson@me.com>
2015-12-31 13:36:20 -0800

    Adds author comment

--- lexico.js
+++ lexico.js
@@ -1,19 +1,23 @@
-// Summary: Write a function to sort an array of strings based on an arbitrary
-// lexicographic ordering. The function will take two parameters: an array of
-// strings to sort and a string specifying the lexicographic order.
+/**
+ * @author Tony Thomson
 
-// Example input #1: ( ["acb", "abc", "bca"], "abc")
-// Example output #1: ["abc","acb","bca"]
+* Summary: Write a function to sort an array of strings based on an arbitrary
+* lexicographic ordering. The function will take two parameters: an array of
+* strings to sort and a string specifying the lexicographic order.
 
-// Example input #2: ( ["acb", "abc", "bca"], "cba")
-// Example output #2: ["bca", "acb", "abc"]
+* Example input #1: ( ["acb", "abc", "bca"], "abc")
+* Example output #1: ["abc","acb","bca"]
 
-// Example input #3: (["aaa","aa",""], "a")
-// Example output #3: ["", "aa", "aaa"]
+* Example input #2: ( ["acb", "abc", "bca"], "cba")
+* Example output #2: ["bca", "acb", "abc"]
 
-// You may assume that the strings to be sorted consist only of characters from
-// the specified lexicographical ordering. You may also assume that the
-// characters in the strings to sort consist only of lowercase a-z.
+* Example input #3: (["aaa","aa",""], "a")
+* Example output #3: ["", "aa", "aaa"]
+
+* You may assume that the strings to be sorted consist only of characters from
+* the specified lexicographical ordering. You may also assume that the
+* characters in the strings to sort consist only of lowercase a-z.
+*/
 
 function lexicoSort(arrayOfStrings, orderStr) {
   'use strict';
